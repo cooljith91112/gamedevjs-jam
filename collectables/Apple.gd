aef396151234aa21af4a658ea11f03ea9434961c
Indrajith K L <mac91112@gmail.com>
2022-04-14 16:36:24 +0530

    Graphics and SFX Changes * Adds Background parallax images (not exactly parallax since we have a fixed camera) * Adds SFX * Fixes Pickable collisions and SFX playback

--- collectables/Apple.gd
+++ collectables/Apple.gd
@@ -4,8 +4,11 @@ export var health=10
 func _ready():
 	$AnimationPlayer.play("idle")
 
-
 func _on_Area2D_body_entered(body):
 	if (body.name=="Player"):
 		body.health += health
-		queue_free()
+		self.hide()
+		$pickup.play()
+
+func _on_pickup_finished():
+	queue_free()
